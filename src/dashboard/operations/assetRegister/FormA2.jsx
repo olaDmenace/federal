9ce883d7ca48e9cc5135672fc6e3adbe2f50323b
olaDmenace <olayinkafagbenro@gmail.com>
2022-10-21 18:32:45 +0100

    Form states of FormA1 and A2 done and add to parent form (FormA)

--- src/dashboard/operations/assetRegister/FormA2.jsx
+++ src/dashboard/operations/assetRegister/FormA2.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function FormA2() {
+function FormA2({ formData, setFormData }) {
     return (
         <div className='py-5 text-primary space-y-3'>
             <h4 className='text-lg font-semibold'>Truck Document</h4>
@@ -10,15 +10,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.refNumber} onChange={(e) => setFormData({ ...formData, refNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.issDate} onChange={(e) => setFormData({ ...formData, issDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.exDate} onChange={(e) => setFormData({ ...formData, exDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -27,15 +27,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.hpRefNumber} onChange={(e) => setFormData({ ...formData, hpRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.hpIssDate} onChange={(e) => setFormData({ ...formData, hpIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.hpExDate} onChange={(e) => setFormData({ ...formData, hpExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -44,15 +44,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.poRefNumber} onChange={(e) => setFormData({ ...formData, poRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.poIssDate} onChange={(e) => setFormData({ ...formData, poIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.poExDate} onChange={(e) => setFormData({ ...formData, poExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -61,15 +61,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.cpRefNumber} onChange={(e) => setFormData({ ...formData, cpRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.cpIssDate} onChange={(e) => setFormData({ ...formData, cpIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.cpExDate} onChange={(e) => setFormData({ ...formData, cpExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -78,15 +78,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.rwRefNumber} onChange={(e) => setFormData({ ...formData, rwRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.rwIssDate} onChange={(e) => setFormData({ ...formData, rwIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.rwExDate} onChange={(e) => setFormData({ ...formData, rwExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset><fieldset>
@@ -94,15 +94,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.hdRefNumber} onChange={(e) => setFormData({ ...formData, hdRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.hdIssDate} onChange={(e) => setFormData({ ...formData, hdIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.hdExDate} onChange={(e) => setFormData({ ...formData, hdExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -111,15 +111,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.aRefNumber} onChange={(e) => setFormData({ ...formData, aRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.aIssDate} onChange={(e) => setFormData({ ...formData, aIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.aExDate} onChange={(e) => setFormData({ ...formData, aExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -128,15 +128,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.cmRefNumber} onChange={(e) => setFormData({ ...formData, cmRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.cmIssDate} onChange={(e) => setFormData({ ...formData, cmIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.cmExDate} onChange={(e) => setFormData({ ...formData, cmExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -145,15 +145,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.irRefNumber} onChange={(e) => setFormData({ ...formData, irRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.irIssDate} onChange={(e) => setFormData({ ...formData, irIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.irExDate} onChange={(e) => setFormData({ ...formData, irExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -162,15 +162,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.tiRefNumber} onChange={(e) => setFormData({ ...formData, tiRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.tiIssDate} onChange={(e) => setFormData({ ...formData, tiIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.tiExDate} onChange={(e) => setFormData({ ...formData, tiExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
@@ -179,15 +179,15 @@ function FormA2() {
                     <fieldset className='grid gap-3 md:grid-cols-2 lg:grid-cols-3 items-end'>
                         <label htmlFor="">
                             Reference Number
-                            <input className='input input-primary w-full' type="text" name="" id="" />
+                            <input value={formData.hdpRefNumber} onChange={(e) => setFormData({ ...formData, hdpRefNumber: e.target.value })} className='input input-primary w-full' type="text" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Issue Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.hdpIssDate} onChange={(e) => setFormData({ ...formData, hdpIssDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                         <label htmlFor="">
                             Expiry Date
-                            <input className='input input-primary w-full' type="date" name="" id="" />
+                            <input value={formData.hdpExDate} onChange={(e) => setFormData({ ...formData, hdpExDate: e.target.value })} className='input input-primary w-full' type="date" name="" id="" />
                         </label>
                     </fieldset>
                 </fieldset>
