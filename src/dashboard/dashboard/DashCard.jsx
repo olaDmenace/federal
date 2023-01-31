d825d7a2e60da73f6e7f7f8bfda1794254612c60
olaDmenace <olayinkafagbenro@gmail.com>
2023-01-31 13:51:19 +0100

    Icons Card fixed

--- src/dashboard/dashboard/DashCard.jsx
+++ src/dashboard/dashboard/DashCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const DashCard = (props) => {
     return (
         <Link to={props.Link} className='bg-[#90D2AF] rounded grid gap-5 text-primary px-5 py-8 justify-items-center hover:text-secondary ease-in duration-300'>
-            <props.icon className='h-40' />
+            <props.icon className='h-12' />
             <p className='text-lg font-semibold'>{props.title}</p>
         </Link>
     )
