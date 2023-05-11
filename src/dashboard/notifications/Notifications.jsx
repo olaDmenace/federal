64e77d9e84d235b20f6e16437e8cb501e2134cb3
olaDmenace <olayinkafagbenro@gmail.com>
2023-05-11 20:15:20 +0100

    Notification Pane Added

--- src/dashboard/notifications/Notifications.jsx
+++ src/dashboard/notifications/Notifications.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Notifications = () => {
     return (
-        <div>Notifications</div>
+        <div>
+            <div>
+                <div className='card rounded-lg shadow-md p-6 grid gap-4'>
+                    <p>Notification <span>12</span></p>
+                    <div className='flex justify-between items-center'>
+                        <div className='space-y-2'>
+                            <p>Journey Management</p>
+                            <p className='text-gray-400'>5 minutes ago</p>
+                        </div>
+                        <span className='indicator-item badge badge-primary'></span>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
