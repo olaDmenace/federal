d6ac24f346208f0cb610f7b1648a5659a9feef67
olaDmenace <olayinkafagbenro@gmail.com>
2023-05-18 15:34:17 +0100

    Notifications Scroll

--- src/dashboard/notifications/NotificationsCard.jsx
+++ src/dashboard/notifications/NotificationsCard.jsx
@@ -3,7 +3,7 @@ import NotificationTypeCard from './NotificationTypeCard'
 
 const NotificationsCard = () => {
     return (
-        <div className='rounded-lg shadow-md p-5 grid gap-5 md:col-span-4 md:h-screen md:overflow-y-scroll bg-white'>
+        <div className='rounded-lg shadow-md p-5 grid gap-5 md:col-span-4 md:h-screen md:overflow-y-scroll scrollbar-thin scrollbar-track-green-100 scrollbar-thumb-green-900 relative bg-white'>
             <p className='font-semibold text-lg'>Notification <span className='indicator-item badge badge-primary float-right'>12</span></p>
             <NotificationTypeCard title={'Journey Management'} time={'5 Minutes Ago'} state={'true'} />
             <NotificationTypeCard title={'Truck Visiblity Alert'} time={'May 06, 2023'} state={'true'} />
