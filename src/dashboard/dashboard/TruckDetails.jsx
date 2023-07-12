b599bb167b6dcc1885e16b14b53932adfce6bed0
olaDmenace <olayinkafagbenro@gmail.com>
2023-07-12 15:51:35 +0100

    Visibility Report Download

--- src/dashboard/dashboard/TruckDetails.jsx
+++ src/dashboard/dashboard/TruckDetails.jsx
@@ -7,6 +7,7 @@ import { PencilIcon } from "@heroicons/react/solid";
 import { useDispatch } from "react-redux";
 // import { truck } fro   m "../../utils/features/truckSlice";
 import { useNavigate } from "react-router-dom";
+import * as xlsx from "xlsx";
 
 const TruckDetails = () => {
   const [trucks, setTrucks] = useState([]);
@@ -39,7 +40,10 @@ const TruckDetails = () => {
     // Fisher-Yates shuffle algorithm
     for (let i = shuffledArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
     }
 
     return shuffledArray;
@@ -56,41 +60,50 @@ const TruckDetails = () => {
     "Outbound",
   ];
 
-  const FUNCStatus = [
-    "Uptime",
-    "Downtime"
-  ]
+  const FUNCStatus = ["Uptime", "Downtime"];
 
-  const Location = [
-    "Osun",
-    "Kaduna",
-    "Kano",
-    "Abuja",
-    "Lagos",
-    "Benin"
-  ]
+  const Location = ["Osun", "Kaduna", "Kano", "Abuja", "Lagos", "Benin"];
   const shuffledValues = (arg) => shuffleArray(arg); // Shuffle the DATstatus array
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const [editTruck, setEditTruck] = useState({})
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [editTruck, setEditTruck] = useState({});
   const onItemClick = (arg) => {
-    setEditTruck(arg)
+    setEditTruck(arg);
     // dispatch(truck())
-    navigate('/dashboard/AssetRegister', { state: { truck: arg } })
+    navigate("/dashboard/AssetRegister", { state: { truck: arg } });
   };
 
-  const [truckId, setTruckId] = useState()
+  const [truckId, setTruckId] = useState();
   const tpf = (arg) => {
-    setTruckId(arg)
-    navigate('/dashboard/TruckProgramming', { state: { truck: arg } })
+    setTruckId(arg);
+    navigate("/dashboard/TruckProgramming", { state: { truck: arg } });
+  };
+
+  // CSV Download of Visibility Report
+  function downloadDataAsCSV() {
+    console.log(trucks);
+    const worksheet = xlsx.utils.json_to_sheet(trucks);
+    const workbook = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet 1");
+    const csvData = xlsx.utils.sheet_to_csv(worksheet);
+
+    const blob = new Blob([csvData], { type: "text/csv" });
+    const downloadUrl = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = downloadUrl;
+    link.download = "Truck Visibility Report.csv";
+    link.click();
   }
 
   return (
     <div className="bg-white p-5 rounded-lg grid gap-5">
       <div className="md:flex justify-between items-center">
         <FormTitle Title={"Truck Visibility Report"} />
-        <button className="btn btn-primary">Download</button>
+        <button className="btn btn-primary" onClick={downloadDataAsCSV}>
+          Download
+        </button>
         {/* <div className="flex gap-5">
           <button className="btn btn-primary">Create</button>
         </div> */}
@@ -110,14 +123,44 @@ const TruckDetails = () => {
           {trucks.map((truck, index) => (
             <div key={truck.truckId} className="table-row">
               <div className="table-cell">{truck.truckNumber}</div>
-              <div className="table-cell">{shuffledValues(Location)[index % shuffledValues(Location).length]}</div>
-              <div className="table-cell">{shuffledValues(FUNCStatus)[index % shuffledValues(FUNCStatus).length]}</div>
+              <div className="table-cell">
+                {
+                  shuffledValues(Location)[
+                    index % shuffledValues(Location).length
+                  ]
+                }
+              </div>
+              <div className="table-cell">
+                {
+                  shuffledValues(FUNCStatus)[
+                    index % shuffledValues(FUNCStatus).length
+                  ]
+                }
+              </div>
               <div className="table-cell">{truck.operationalStatus}</div>
-              <div className="table-cell">{shuffledValues(DATstatus)[index % shuffledValues(DATstatus).length]}</div>
-              <div className="table-cell">{formatDate(truck.manufactureDate)}</div>
+              <div className="table-cell">
+                {
+                  shuffledValues(DATstatus)[
+                    index % shuffledValues(DATstatus).length
+                  ]
+                }
+              </div>
+              <div className="table-cell">
+                {formatDate(truck.manufactureDate)}
+              </div>
               <div className="flex gap-5 items-center">
-                <div className="table-cell"><PencilIcon className="h-6 border border-primary rounded-sm cursor-pointer" onClick={() => onItemClick(truck)} /></div>
-                <div className="table-cell"><TruckIcon className="h-6 border border-primary rounded-sm cursor-pointer" onClick={() => tpf(truck)} /></div>
+                <div className="table-cell">
+                  <PencilIcon
+                    className="h-6 border border-primary rounded-sm cursor-pointer"
+                    onClick={() => onItemClick(truck)}
+                  />
+                </div>
+                <div className="table-cell">
+                  <TruckIcon
+                    className="h-6 border border-primary rounded-sm cursor-pointer"
+                    onClick={() => tpf(truck)}
+                  />
+                </div>
               </div>
             </div>
           ))}
