ad8e3a8d3267beb047de2f5e1af2b059482b1b55
olaDmenace <olayinkafagbenro@gmail.com>
2024-06-01 18:20:42 +0100

    pagetitle rename

--- src/Membership/Login/Login.js
+++ src/Membership/Login/Login.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import LoginForm from './LoginForm'
-import PageTitle from '../../utils/PageTitle'
-
+import React from "react";
+import LoginForm from "./LoginForm";
+import PageTitle from "../../utils/PageTitle";
 
 function Login() {
-  PageTitle('A&C Login')
+  PageTitle("FIRS Login");
   return (
-    <div className='space-y-10 px-5'>
-      <h2 className='text-4xl font-bold'>Welcome Back.</h2>
-      <div className='mx-auto place-self-center'>
-        <LoginForm/>
+    <div className="space-y-10 px-5">
+      <h2 className="text-4xl font-bold">Welcome Back.</h2>
+      <div className="mx-auto place-self-center">
+        <LoginForm />
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
